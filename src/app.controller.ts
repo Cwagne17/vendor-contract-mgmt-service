745376c17cd9d61a6355a7b2f54c579e49e73157
Christopher M Wagner <70287489+Cwagne17@users.noreply.github.com>
2022-05-03 13:11:42 -0400

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import version = require('../package.json').version;
 
 @Controller()
 export class AppController {
 
     @Get() 
     getVersion() {
-        return { message: `Welcome to the Church of Philippi Vendor Contract Management Service Version ${require('../package.json').version}`};
+        return { message: `Welcome to the Church of Philippi Vendor Contract Management Service Version ${version}`};
     }
 }
